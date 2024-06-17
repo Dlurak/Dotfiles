6aeeaa8ed729b9de7aeb1ee9cba1169b66283915
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-06-17 19:07:06 +0200

    Add a new command to nvim

--- nvim/lua/custom/commands.lua
+++ nvim/lua/custom/commands.lua
@@ -43,3 +43,12 @@ vim.api.nvim_create_user_command('Blame', function()
 	local command = string.format('!git blame %s -L%d,+1', filename, line)
 	vim.cmd(command)
 end, { nargs = 0, desc = "Git blame the current line" })
+
+-- Copy text to clipboard using codeblock format ```{ft}{content}```
+vim.api.nvim_create_user_command('CopyCodeBlock', function(opts)
+  local lines = vim.api.nvim_buf_get_lines(0, opts.line1 - 1, opts.line2, true)
+  local content = table.concat(lines, '\n')
+  local result = string.format('```%s\n%s\n```', vim.bo.filetype, content)
+  vim.fn.setreg('+', result)
+  vim.notify 'Text copied to clipboard'
+end, { range = true })
